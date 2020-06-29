b2734ec59db61b380df1f84b9a0638e5f63e4a6c
Sergey Zorin <sergey.zorin@outlook.com>
2020-06-29 14:40:33 +0300

    Improve UI.

--- src/DemoApp/MainWindow.xaml.cs
+++ src/DemoApp/MainWindow.xaml.cs
@@ -19,5 +19,39 @@ namespace DemoApp
 
             FileTreeMapControl.DirectoryPath = Directory.GetParent(FileTreeMapControl.DirectoryPath)?.FullName;
         }
+
+        private void PathTextBox_Focus(object sender, RoutedEventArgs e)
+        {
+            UpdatePlaceholderTextVisibility();
+        }
+
+        private void PathTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdatePlaceholderTextVisibility();
+        }
+
+        private void UpdatePlaceholderTextVisibility()
+        {
+            if (PathTextBox.IsFocused)
+            {
+                PlaceholderText.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                PlaceholderText.Visibility = string.IsNullOrEmpty(PathTextBox.Text)
+                    ? Visibility.Visible
+                    : Visibility.Hidden;
+            }
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            
+            if (dialog.ShowDialog() == true)
+            {
+                PathTextBox.Text = dialog.SelectedPath;
+            }
+        }
     }
 }
